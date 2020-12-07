aa4537ec200f08beab6fd0e53c8aafc18498eadd
Edvinas Zubavicius <edwin.zubowicz@gmail.com>
2020-12-08 01:27:37 +0200

    signup and signin pages changed

--- front/src/components/SignInPage.js
+++ front/src/components/SignInPage.js
@@ -2,17 +2,21 @@ import React from 'react';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
+import InputLabel from "@material-ui/core/InputLabel";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import { Route, BrowserRouter as Router } from "react-router-dom"
-import SignUpPage from "./SignUpPage"
 
 const useStyles = makeStyles((theme) => ({
+    formControl: {
+        marginTop: theme.spacing(2),
+        minWidth: 150
+    },
     paper: {
         marginTop: theme.spacing(8),
         display: 'flex',
@@ -68,6 +72,20 @@ function SignInPage() {
                         id="password"
                         autoComplete="current-password"
                     />
+                    <FormControl variant="outlined" className={classes.formControl}>
+                        <InputLabel id="demo-simple-select-outlined-label">Role</InputLabel>
+                        <Select
+                            labelId="demo-simple-select-outlined-label"
+                            id="demo-simple-select-outlined"
+                            label="Role"
+                        >
+                            <MenuItem value="">
+                                <em>None</em>
+                            </MenuItem>
+                            <MenuItem value={1}>Developer</MenuItem>
+                            <MenuItem value={2}>Employer</MenuItem>
+                        </Select>
+                    </FormControl>
                     <Button
                         type="submit"
                         fullWidth
--- front/src/components/SignUpPage.js
+++ front/src/components/SignUpPage.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import InputLabel from "@material-ui/core/InputLabel";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
 
 const useStyles = makeStyles((theme) => ({
+    formControl: {
+        minWidth: 150
+    },
     paper: {
         marginTop: theme.spacing(8),
         display: 'flex',
@@ -94,6 +96,22 @@ function SignUpPage() {
                                 autoComplete="current-password"
                             />
                         </Grid>
+                        <Grid item xs={12}>
+                            <FormControl variant="outlined" className={classes.formControl}>
+                                <InputLabel id="demo-simple-select-outlined-label">Role</InputLabel>
+                                <Select
+                                    labelId="demo-simple-select-outlined-label"
+                                    id="demo-simple-select-outlined"
+                                    label="Role"
+                                >
+                                    <MenuItem value="">
+                                        <em>None</em>
+                                    </MenuItem>
+                                    <MenuItem value={1}>Developer</MenuItem>
+                                    <MenuItem value={2}>Employer</MenuItem>
+                                </Select>
+                            </FormControl>
+                        </Grid>
                     </Grid>
                     <Button
                         type="submit"
@@ -103,12 +121,12 @@ function SignUpPage() {
                         className={classes.submit}
                     >
                         Sign Up
-          </Button>
+                    </Button>
                     <Grid container justify="flex-end">
                         <Grid item>
                             <Link href="#" variant="body2">
                                 Already have an account? Sign in
-              </Link>
+                            </Link>
                         </Grid>
                     </Grid>
                 </form>
