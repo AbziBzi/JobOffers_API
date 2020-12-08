de882ee38d228e43e708e4df8ddd1c7c83cec627
Edvinas Zubavicius <edwin.zubowicz@gmail.com>
2020-12-08 02:10:43 +0200

    changed project structure

--- front/src/index.js
+++ front/src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import SignInPage from './components/SignInPage'
-import SignUpPage from './components/SignUpPage'
+import SignInPage from './pages/SignInPage'
+import SignUpPage from './pages/SignUpPage'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
