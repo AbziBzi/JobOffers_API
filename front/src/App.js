b7274241b827d33057e3f2feae623c8160dbd183
Edvinas Zubavicius <edwin.zubowicz@gmail.com>
2020-12-09 15:06:11 +0200

    added companyCard template

--- front/src/App.js
+++ front/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 import Container from '@material-ui/core/Container';
-import SignUpPage from './components/SignUpPage'
+import SignUpPage from './pages/SignUpPage'
+import JobOfferCard from './components/JobOfferCard'
 
 function App() {
   return (
     <div className="App">
       <Container maxWidth="md" >
-        <SignUpPage />
+        <CompanyCard />
       </Container>
     </div>
   );
