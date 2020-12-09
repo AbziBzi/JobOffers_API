6cdfe62ecf094a1b753000d5bf6a1ec81597d3a6
mantasmak <maksimaviciaus@gmail.com>
2020-12-09 14:04:29 +0100

    Add API call.

--- front/src/App.js
+++ front/src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './App.css';
 import Container from '@material-ui/core/Container';
 import SignUpPage from './components/SignUpPage'
+import JobOfferPage from './components/JobOfferPage';
 
 function App() {
   return (
     <div className="App">
       <Container maxWidth="md" >
-        <SignUpPage />
+        <JobOfferPage />
       </Container>
     </div>
   );
