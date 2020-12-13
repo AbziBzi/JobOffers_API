9d2162ab5ff94f565946812994897663dc7c7efe
mantasmak <maksimaviciaus@gmail.com>
2020-12-13 15:50:26 +0100

    Pre merge

--- front/src/App.js
+++ front/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import JobOfferList from './components/JobOfferList';
-import JobOfferPage from './components/JobOfferPage';
 import CompaniesPage from './pages/CompaniesPage';
 import CompanyPage from './pages/CompanyPage';
 import NavBar from './components/NavBar'
@@ -15,7 +14,6 @@ function App() {
           <Route path="/companies" exact component={CompaniesPage} />
           <Route path="/companies/:id" exact component={CompanyPage} />
           <Route path="/jobs" exact component={JobOfferList} />
-          <Route path="/job" exact component={JobOfferPage} />
         </Switch>
       </BrowserRouter>
     </div>
