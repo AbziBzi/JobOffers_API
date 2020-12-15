88ffb259f461095be6a5dec0213ffdd85bac953d
Edvinas Zubavicius <edwin.zubowicz@gmail.com>
2020-12-15 14:54:31 +0100

    some changes in navBar

--- front/src/components/NavBar.js
+++ front/src/components/NavBar.js
@@ -1,65 +1,39 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
+import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
+import Box from '@material-ui/core/Box';
+import { UserContext } from '../UserContext';
+
 
 const useStyles = makeStyles((theme) => ({
     root: {
         flexGrow: 1,
-    },
-    menuButton: {
-        marginRight: theme.spacing(2),
-    },
-    title: {
-        marginLeft: 145,
-        flexGrow: 1,
+        backgroundColor: theme.palette.background.paper,
     },
 }));
 
-function NavBar() {
+export default function SimpleTabs() {
+    const user = useContext(UserContext)
     const classes = useStyles();
-    const [anchorEl, setAnchorEl] = React.useState(null);
+    const [value, setValue] = React.useState(0);
 
-    const handleMenuClick = (event) => {
-        setAnchorEl(event.currentTarget);
-    };
-
-    const handleClose = () => {
-        setAnchorEl(null);
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
     };
 
     return (
         <div className={classes.root}>
             <AppBar position="static">
-                <Toolbar>
-                    <IconButton onClick={handleMenuClick} edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                        <MenuIcon />
-                    </IconButton>
-                    <Menu
-                        id="simple-menu"
-                        anchorEl={anchorEl}
-                        keepMounted
-                        open={Boolean(anchorEl)}
-                        onClose={handleClose}
-                    >
-                        <MenuItem onClick={handleClose}>Profile</MenuItem>
-                        <MenuItem onClick={handleClose}>My account</MenuItem>
-                        <MenuItem onClick={handleClose}>Logout</MenuItem>
-                    </Menu>
-                    <Typography variant="h6" className={classes.title}>
-                        NavBar
-                    </Typography>
-                    <Button color="inherit">Log out</Button>
-                </Toolbar>
+                <Tabs value={value} onChange={handleChange} aria-label="simple tabs example">
+                    <Tab label="Profile" href="/login" />
+                    <Tab label="Open Jobs" />
+                    <Tab label="" />
+                </Tabs>
             </AppBar>
         </div>
     );
 }
-
-export default NavBar
